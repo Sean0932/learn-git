b224531c88fea921a56c5570533826ca9f59a119
Sean0932 <ysy0932@sina.com>
2020-03-28 22:41:32 +0800

    new file 4

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 print('Hello world!')
 
 print('Sean0932')
+
+print('Hello world!S')
--- today.py
+++ today.py
@@ -2,3 +2,5 @@ from datetime import datetime
 print(datetime.now().strftime('今天是%Y年%m月%d日'))
 
 print('Sean0932')
+
+print('Hello would!')
